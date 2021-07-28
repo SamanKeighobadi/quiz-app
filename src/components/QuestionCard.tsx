145bc002de40f30f38c0491b12954f6b7f212bd4
saman keighobadi <saman.keighobadi@gmail.com>
2021-07-28 19:15:13 +0430

    editing Question Card

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,11 +1,31 @@
-import React from 'react';
+import React from "react";
 
-const QuestionCard = () => {
-    return (
-        <div>
-            QuestionCard
-        </div>
-    );
+type Props = {
+  question: string;
+  answer: string[];
+  callback: any[];
+  userAnswer: string;
+  questionNumber: string;
+  totlaQuestions: number;
+};
+
+const QuestionCard: React.FC<Props> = ({
+  answer,
+  callback,
+  question,
+  questionNumber,
+  totlaQuestions,
+  userAnswer,
+  children,
+}) => {
+  return (
+      <div>
+          <p className="number">
+              Question: {questionNumber} / {totlaQuestions}
+          </p>
+          <p dangerouslySetInnerHTML={{__html: question}} 
+      </div>
+  );
 };
 
 export default QuestionCard;
