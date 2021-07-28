c0a261886fad9620c35a4dac6168b21d98c979ff
saman keighobadi <saman.keighobadi@gmail.com>
2021-07-29 01:53:55 +0430

    set up states and props

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,51 @@ import axios from 'axios';
 import React,{useState,useEffect} from 'react';
 import QuestionCard from './components/QuestionCard';
 
+const TOTAL_QUESTIONS = 10;
 
 function App() {
 
-  const QUIZ_API = `https://opentdb.com/api.php?amount=10&type=multiple`;
+  const [loading,setLoading] = useState(false)
+  const [questions,setQuestions] = useState([])
+  const [number,setNumber] = useState(0);
+  const [userAnswer,setUserAnswer] = useState([]);
+  const [score,setScore] = useState(0);
+  const [gameOver,setGameOver]= useState(true);
+
+  // const QUIZ_API = `https://opentdb.com/api.php?amount=10&type=multiple`;
+
+  const  startTraivia = async() => {
+
+  }
+
+  const cheeckAnswer = (e:React.MouseEvent<HTMLButtonElement>) => {
+
+  }
+
+  const nextQuestion = () => {
+
+  }
+
+  useEffect(() =>{})
 
   return (
     <div className="App">
-      Hello World
-      <QuestionCard />
+      <h1>Quiz App</h1>
+      <button className="start" onClick={startTraivia}>
+        start
+      </button>
+      <p className="score">Score: </p>
+      <QuestionCard 
+        questionNumber={number + 1}
+        totlaQuestions={TOTAL_QUESTIONS}
+        question={questions[number].question}
+        answers={questions[number].answers}
+        userAnswer={userAnswer ? userAnswer[number] : undefined}
+        callback={cheeckAnswer}
+      />
+      <button className="next" onClick={nextQuestion} >
+        Next Questions
+      </button>
     </div>
   );
 }
