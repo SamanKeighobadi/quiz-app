c14ad4ce1b617957d2b10a396017289e757254e5
saman keighobadi <saman.keighobadi@gmail.com>
2021-07-28 15:55:46 +0430

    create init files

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import axios from 'axios';
+import React,{useState,useEffect} from 'react';
+import QuestionCard from './components/QuestionCard';
 
 
 function App() {
+
+  const QUIZ_API = `https://opentdb.com/api.php?amount=10&type=multiple`;
+
   return (
     <div className="App">
       Hello World
+      <QuestionCard />
     </div>
   );
 }
